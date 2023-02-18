3cff27abb37728c7c75c99830943c88b55d095b0
marufbroh <marufmd775@gmail.com>
2023-02-18 19:25:54 +0600

    validation done

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ function showData(geometryName, geometryResult) {
     <td><button class="btn btn-sm btn-primary">Convert to m<sup>2</sup></button></td>
     
     `
-    tableToday.appendChild(tr)
+    tableToday.appendChild(tr);
 }
 
 // Get Input Value Function
@@ -21,6 +21,7 @@ function getInputValueById(inputFieldId) {
     const inputField = document.getElementById(inputFieldId);
     const inputValueString = inputField.value;
     const inputValue = parseFloat(inputValueString);
+    inputField.value = '';
     return inputValue;
 }
 
@@ -35,18 +36,18 @@ function getElementValueById(elementId) {
 function randomColor() {
     let color = [];
     for (let i = 0; i < 3; i++) {
-      color.push(Math.floor(Math.random() * 256));
+        color.push(Math.floor(Math.random() * 256));
     }
     return 'rgb(' + color.join(', ') + ')';
-  }
+};
 
 // card body
 const allcardBody = document.getElementsByClassName('card-random-color');
-for(const cardBody of allcardBody){
-    cardBody.addEventListener('mouseover', function(event){
+for (const cardBody of allcardBody) {
+    cardBody.addEventListener('mouseover', function () {
         cardBody.style.backgroundColor = randomColor();
     })
-}
+};
 
 
 // Triangle Button Add Event Listener
@@ -54,7 +55,14 @@ document.getElementById('triangle-btn').addEventListener('click', function () {
     serial += 1;
     const triangleName = document.getElementById('triangle-name').innerText;
     const triangleBase = getInputValueById('triangle-base');
-    const triangleHeight = getInputValueById('triangle-height')
+    const triangleHeight = getInputValueById('triangle-height');
+
+    if (isNaN(triangleBase) || typeof (triangleBase) !== 'number' || triangleBase <= 0) {
+        return alert('please provide valid numbers')
+    }
+    else if (isNaN(triangleHeight) || typeof (triangleHeight) !== 'number' || triangleHeight <= 0) {
+        return alert('please provide valid numbers')
+    }
 
     const triangleArea = (0.5 * triangleBase * triangleHeight).toFixed(2);
 
@@ -68,6 +76,13 @@ document.getElementById('rectangle-btn').addEventListener('click', function () {
     const rectangleName = document.getElementById('rectangle-name').innerText;
     const rectangleWidth = getInputValueById('rectangle-width');
     const rectangleLength = getInputValueById('rectangle-length');
+
+    if (isNaN(rectangleWidth) || typeof (rectangleWidth) !== 'number' || rectangleWidth <= 0) {
+        return alert('please provide valid numbers')
+    }
+    else if (isNaN(rectangleLength) || typeof (rectangleLength) !== 'number' || rectangleLength <= 0) {
+        return alert('please provide valid numbers')
+    }
 
     const rectangleArea = (rectangleWidth * rectangleLength).toFixed(2);
 
@@ -94,7 +109,7 @@ document.getElementById('rhombus-btn').addEventListener('click', function () {
     const rhombusName = document.getElementById('rhombus-name').innerText;
     const rhombusArea = (0.5 * 16 * 8).toFixed(2);
 
-    showData(rhombusName, rhombusArea)
+    showData(rhombusName, rhombusArea);
 })
 
 
@@ -104,6 +119,13 @@ document.getElementById('pentagon-btn').addEventListener('click', function () {
     const pentagonName = document.getElementById('pentagon-name').innerText;
     const pentagonPerimeter = getInputValueById('pentagon-perimeter');
     const pentagonApothem = getInputValueById('pentagon-apothem');
+
+    if (isNaN(pentagonPerimeter) || typeof (pentagonPerimeter) !== 'number' || pentagonPerimeter <= 0) {
+        return alert('please provide valid numbers')
+    }
+    else if (isNaN(pentagonApothem) || typeof (pentagonApothem) !== 'number' || pentagonApothem <= 0) {
+        return alert('please provide valid numbers')
+    }
 
     const pentagonArea = (0.5 * pentagonPerimeter * pentagonApothem).toFixed(2);
 
@@ -117,6 +139,13 @@ document.getElementById('ellipse-btn').addEventListener('click', function () {
     const ellipseName = document.getElementById('ellipse-name').innerText;
     const ellipseInput1 = getInputValueById('ellipse-input-1');
     const ellipseInput2 = getInputValueById('ellipse-input-2');
+
+    if (isNaN(ellipseInput1) || typeof (ellipseInput1) !== 'number' || ellipseInput1 <= 0) {
+        return alert('please provide valid numbers')
+    }
+    else if (isNaN(ellipseInput2) || typeof (ellipseInput2) !== 'number' || ellipseInput2 <= 0) {
+        return alert('please provide valid numbers')
+    }
 
     const ellipseArea = (3.14 * ellipseInput1 * ellipseInput2).toFixed(2);
 
