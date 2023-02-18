cec5a9c3325ee5a944dfeeb935f6c4327d8ffa45
marufbroh <marufmd775@gmail.com>
2023-02-18 15:33:21 +0600

    showData Function complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log('index js added')
+let serial = 0;
+
+// Show Calculation Data Function
+function showData(geometryName, geometryResult) {
+    const tableToday = document.getElementById('table-body');
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    
+    <td>${serial}</td>
+    <td>${geometryName}</td>
+    <td>${geometryResult}m<sup>2</sup></td>
+    <td><button class="btn btn-sm">Convert to m<sup>2</sup></button></td>
+    
+    `
+    tableToday.appendChild(tr)
+}
+
+
